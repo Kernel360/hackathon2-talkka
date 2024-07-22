27f827289cfff529bd3562ec6cb1be8b5ce1788e
Photogrammer <khsj15@khu.ac.kr>
2024-07-22 12:19:48 +0900

    chore: 프로젝트 초기 설정
    Spring Boot 모듈 생성
    - 서버 모듈 생성
    
    IntelliJ Code Style 관련 설정 공유
    - naver-hackday formatter 추가
    - naver-hackday checkstyle 추가
    개발용 Docker compose 작성

---
+++ bus-server/src/main/java/org/kernel360/busclientserver/BusClientServerApplication.java
@@ -0,0 +1,13 @@
+package org.kernel360.busclientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusClientServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusClientServerApplication.class, args);
+	}
+
+}
---
+++ bus-server/src/test/java/org/kernel360/busclientserver/BusClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kernel360.busclientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusClientServerApplicationTests {
+
+	@Test
+	public void test() {
+	}
+
+}
