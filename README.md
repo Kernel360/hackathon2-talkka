74ff14006ae0b0cf010c312d7401178ca2ac3051
Photogrammer <khsj15@khu.ac.kr>
2024-07-22 12:50:42 +0900

    feat: 공공 데이터 csv 파일 DB Insert 구현
    LOAD DATA 를 통한 INSERT 구현
    SCHEMA 스크립트 작성
    DB 초기 데이터 로드 스크립트 작성

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hackathon2-gyeonggiBus
+- MySQL 기초 DB 세팅
+```shell
+$ mysql -u root -p
+# 비밀번호 입력
+$ source /var/lib/mysql-files/init_db.sql
+# schema 초기화 확인 
+```
---
+++ data/mysql-files/bulk_insert_stations.sql
@@ -0,0 +1,12 @@
+LOAD DATA INFILE '/var/lib/mysql-files/bus_station.csv'
+    INTO TABLE BUS_STATION
+    FIELDS TERMINATED BY ',' -- 필드 구분자
+    ESCAPED BY '"'
+    ENCLOSED BY '"' -- 텍스트 필드가 큰따옴표로 감싸져 있을 경우
+    LINES TERMINATED BY '\n' -- 줄 구분자
+    IGNORE 1 ROWS -- 첫 번째 행(헤더)을 무시
+
+    (@station_id, @station_name, @coordinate_x, @coordinate_y, @station_type, @station_eng_name, @registered_at)
+    SET station_id = @station_id, station_name = @station_name, station_location =
+            ST_PointFromText(CONCAT('POINT(', @coordinate_y, ' ', @coordinate_x, ')')), station_name_en = @station_eng_name, station_type = @station_type
+;
