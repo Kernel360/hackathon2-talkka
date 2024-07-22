d47173f3a0680a7ca2ee444f92903b73075ed858
Photogrammer <khsj15@khu.ac.kr>
2024-07-22 17:12:54 +0900

    fix: fix bus_station bulk insert code

--- data/mysql-files/bulk_insert_stations.sql
+++ data/mysql-files/bulk_insert_stations.sql
@@ -6,7 +6,12 @@ LOAD DATA INFILE '/var/lib/mysql-files/bus_station.csv'
     LINES TERMINATED BY '\n' -- 줄 구분자
     IGNORE 1 ROWS -- 첫 번째 행(헤더)을 무시
 
-    (@station_id, @station_name, @coordinate_x, @coordinate_y, @station_type, @station_eng_name, @registered_at)
-    SET station_id = @station_id, station_name = @station_name, station_location =
-            ST_PointFromText(CONCAT('POINT(', @coordinate_y, ' ', @coordinate_x, ')')), station_name_en = @station_eng_name, station_type = @station_type
+    (@station_id, @station_name, @station_type, @station_eng_name, @registered_at, @latitude, @longitude)
+    SET station_id = @station_id,
+        station_name = @station_name,
+        station_type = @station_type,
+        station_name_en = @station_eng_name,
+        registered_at = @registered_at,
+        latitude = @latitude,
+        longitude = @longitude
 ;
