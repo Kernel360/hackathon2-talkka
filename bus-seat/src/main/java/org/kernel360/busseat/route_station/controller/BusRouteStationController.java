6d1755d3d072b729337de0354c22bb5f0c5c6415
Photogrammer <khsj15@khu.ac.kr>
2024-07-22 20:17:00 +0900

    feat: 경유 정류장 JPA 연동 및 기본 서비스 구현

---
+++ bus-seat/src/main/java/org/kernel360/busseat/route_station/controller/BusRouteStationController.java
@@ -0,0 +1,39 @@
+package org.kernel360.busseat.route_station.controller;
+
+import java.util.List;
+
+import org.kernel360.busseat.route_station.dto.RouteStationDto;
+import org.kernel360.busseat.route_station.service.BusRouteStationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/route-stations")
+@RequiredArgsConstructor
+public class BusRouteStationController {
+	private final BusRouteStationService busRouteStationService;
+
+	@GetMapping("/{routeStationId}")
+	public RouteStationDto findById(@PathVariable("routeStationId") Long routeStationId) {
+		return busRouteStationService.findById(routeStationId).orElse(null);
+	}
+
+	@GetMapping("/")
+	public List<RouteStationDto> findAll() {
+		return busRouteStationService.findAll();
+	}
+
+	@GetMapping("/route/{routeId}")
+	public List<RouteStationDto> findByRouteId(@PathVariable("routeId") Long routeId) {
+		return busRouteStationService.findByRouteId(routeId);
+	}
+
+	@GetMapping("/route/{routeId}/seq/{seq}")
+	public RouteStationDto findByRouteIdAndSeq(@PathVariable("routeId") Long routeId, @PathVariable("seq") Short seq) {
+		return busRouteStationService.findByRouteIdAndSeq(routeId, seq).orElse(null);
+	}
+}
---
+++ bus-seat/src/main/java/org/kernel360/busseat/route_station/dto/RouteStationDto.java
@@ -0,0 +1,28 @@
+package org.kernel360.busseat.route_station.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RouteStationDto {
+	private Long busRouteStopId;
+
+	private Long routeId;
+
+	private Long stationId;
+
+	private Short stationSeq;
+
+	private String stationName;
+
+	private String regionName;
+
+	private String districtCd;
+}
