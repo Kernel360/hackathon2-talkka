77be7fc6f7ba055434096c55675e382ba6edd324
song <shd2768@naver.com>
2024-07-23 12:37:34 +0900

    feat: 사용자 요청 insert 연관관계 추가
    bus_route 테이블과 user_collect_request 테이블의 연관관계 설정
    
    Closes #32

--- bus-seat/src/main/java/org/kernel360/busseat/user_request/service/UserRequestService.java
+++ bus-seat/src/main/java/org/kernel360/busseat/user_request/service/UserRequestService.java
@@ -2,6 +2,8 @@ package org.kernel360.busseat.user_request.service;
 
 import java.sql.Timestamp;
 
+import org.kernel360.busseat.route.entity.BusRouteEntity;
+import org.kernel360.busseat.route.repository.BusRouteRepository;
 import org.kernel360.busseat.user_request.dto.UserRequestDto;
 import org.kernel360.busseat.user_request.entity.UserRequestEntity;
 import org.kernel360.busseat.user_request.repository.UserRequestRepository;
@@ -15,9 +17,14 @@ public class UserRequestService {
 
 	private final UserRequestRepository userRequestRepository;
 
+	private final BusRouteRepository busRouteRepository;
+
 	public UserRequestDto create(UserRequestDto userRequestDto) {
 
 		Timestamp now = new Timestamp(System.currentTimeMillis());
+
+		// 추후 검증을 위한 코드(현재는 실질적으로 사용 x)
+		BusRouteEntity busRouteEntity = busRouteRepository.findById(String.valueOf(userRequestDto.getRouteId())).get();
 
 		var entity = UserRequestEntity.builder()
 			.routeId(userRequestDto.getRouteId())
@@ -34,6 +41,7 @@ public class UserRequestService {
 	private UserRequestDto toDto(UserRequestEntity userRequestEntity) {
 		return UserRequestDto.builder()
 			.routeId(userRequestEntity.getRouteId())
+			.routeName(userRequestEntity.getRouteName())
 			.build();
 	}
 }
