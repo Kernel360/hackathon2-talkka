f4fc9d701e25c11129b7261532ff1087bb8e1c91
Photogrammer <khsj15@khu.ac.kr>
2024-07-25 15:23:39 +0900

    fix: CORS bean 문제

--- bus-seat/src/main/java/org/kernel360/busseat/WebConfig.java
+++ bus-seat/src/main/java/org/kernel360/busseat/WebConfig.java
@@ -1,24 +1,26 @@
 package org.kernel360.busseat;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins(
-					"http://localhost:8081",
-					"https://test.photogrammer.me",
-					"https://bus.photogrammer.me"
-				); // allow react dev
+			.allowedOrigins(
+				"http://localhost:8081",
+				"https://test.photogrammer.me",
+				"https://bus.photogrammer.me"
+			); // allow react dev
 	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/static/");
+			.addResourceLocations("classpath:/static/");
 	}
 }
