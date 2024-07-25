bee907780a62be05a5740afe7a890b7cd2721196
Photogrammer <khsj15@khu.ac.kr>
2024-07-25 15:01:20 +0900

    fix: URL 오류 변경 / docker

--- frontend/src/api/GET_requestRouteList.tsx
+++ frontend/src/api/GET_requestRouteList.tsx
@@ -1,42 +1,42 @@
-import { API_URL } from "./API_URL";
+import {API_URL} from "./API_URL";
 
 export type Route = {
-  routeId: string,
-  routeName: string,
-  stationStart: string,
-  stationEnd: string,
-  regionName: string,
+    routeId: string,
+    routeName: string,
+    stationStart: string,
+    stationEnd: string,
+    regionName: string,
 }
 
 export interface GET_paginationResponseFormat {
-  pagination: {
-    currentPage: number,
-    totalPage: number,
-  },
-  routes: Route[],
+    pagination: {
+        currentPage: number,
+        totalPage: number,
+    },
+    routes: Route[],
 }
 
 export async function GET_requestRoutesPaginated(
-  pageNumber: number,
-  pageSize: number,
-  routeSearchQuery: string,
+    pageNumber: number,
+    pageSize: number,
+    routeSearchQuery: string,
 ) {
 
-  // /api/route/registered?search={keyword}&page=0&size=10
-  let requestUrl = `${API_URL}/route/registerd?search=${routeSearchQuery}&page=${pageNumber}&size=${pageSize}`;
+    // /api/route/registered?search={keyword}&page=0&size=10
+    let requestUrl = `${API_URL}/route/registered?search=${routeSearchQuery}&page=${pageNumber}&size=${pageSize}`;
 
-  const getRoutesReponse = await fetch(requestUrl, {
-    method: 'GET',
-  });
+    const getRoutesReponse = await fetch(requestUrl, {
+        method: 'GET',
+    });
 
 
-  // on error
-  if (!getRoutesReponse.ok) {
-    const errorData = await getRoutesReponse.json();
-    alert(errorData);
-    return null;
-  }
+    // on error
+    if (!getRoutesReponse.ok) {
+        const errorData = await getRoutesReponse.json();
+        alert(errorData);
+        return null;
+    }
 
-  const body: GET_paginationResponseFormat = await getRoutesReponse.json();
-  return body;
+    const body: GET_paginationResponseFormat = await getRoutesReponse.json();
+    return body;
 }
